efc4918665d059ca5ba09d32997184898bb09e5a
Damiano Enerli <damiano.enerli@loanxchain.com>
2023-05-16 13:17:09 +0200

    Add failing test cases (normal approximation too rough)

--- src/lib/Finance.sol
+++ src/lib/Finance.sol
@@ -6,6 +6,7 @@ import "forge-std/console.sol";
 import {FixedPointMathLib} from "./FixedPointMathLib.sol";
 import {AmountsMath} from "./AmountsMath.sol";
 import {SignedMath} from "./SignedMath.sol";
+import {Normal} from "./Normal.sol";
 
 /// @title Implementation of core financial computations for Smilee protocol
 library Finance {
@@ -27,24 +28,26 @@ library Finance {
         uint256 tau;
     }
 
+    struct DeltaIGComponents {
+        uint256 c1;
+        uint256 c2;
+        uint256 c3;
+        uint256 c4;
+        uint256 c5;
+        uint256 c6;
+        uint256 c7;
+    }
+
     function igDeltas(DeltaIGParams memory params) public pure returns (int256 igDUp, int256 igDDown) {
         (int256 d1_, int256 d2_, int256 d3_, uint256 sigmaTaurtd) = ds(params);
         uint256 sigmaTaurtdPi2rtd = sigmaTaurtd.wmul(PI2_RTD);
-        uint256 bo = _bo(params);
         {
-            (uint256 c1_, uint256 c2_, uint256 c3_, uint256 c4_, uint256 c5_) = cs(
-                params,
-                d1_,
-                d2_,
-                d3_,
-                sigmaTaurtdPi2rtd
-            );
-
-            uint256 c123 = c1_ + c2_ + c3_;
-            uint256 c45 = c4_ + c5_;
+            DeltaIGComponents memory cs_ = cs(params, d1_, d2_, d3_, sigmaTaurtdPi2rtd);
+            uint256 c123 = cs_.c1 + cs_.c2 + cs_.c3;
+            uint256 c45 = cs_.c4 + cs_.c5;
             igDUp = int256(c123) + SignedMath.neg(c45);
+            igDDown = int256(cs_.c6) - int256(cs_.c7) - igDUp;
         }
-        igDDown = int256(AmountsMath.wrap(1).wdiv(2 * params.K)) - int256(bo / 2) - igDUp;
     }
 
     function cs(
@@ -53,13 +56,16 @@ library Finance {
         int256 d2_,
         int256 d3_,
         uint256 sigmaTaurtdPi2rtd
-    ) public pure returns (uint256 c1_, uint256 c2_, uint256 c3_, uint256 c4_, uint256 c5_) {
+    ) public pure returns (DeltaIGComponents memory cs_) {
         uint256 bo = _bo(params);
-        c1_ = c1(params.r, params.tau, d2_, params.S, sigmaTaurtdPi2rtd);
-        c2_ = c2(params.K, d1_);
-        c3_ = c3(d1_, params.K, sigmaTaurtdPi2rtd);
-        c4_ = c4(bo, d3_);
-        c5_ = c5(bo, d3_, sigmaTaurtdPi2rtd);
+        uint256 c1_ = c1(params.r, params.tau, d2_, params.S, sigmaTaurtdPi2rtd);
+        uint256 c2_ = c2(params.K, d1_);
+        uint256 c3_ = c3(d1_, params.K, sigmaTaurtdPi2rtd);
+        uint256 c4_ = c4(bo, d3_);
+        uint256 c5_ = c5(bo, d3_, sigmaTaurtdPi2rtd);
+        uint256 c6_ = c6(params.K);
+        uint256 c7_ = c7(bo);
+        return DeltaIGComponents(c1_, c2_, c3_, c4_, c5_, c6_, c7_);
     }
 
     function ds(
@@ -113,16 +119,16 @@ library Finance {
         uint256 S,
         uint256 sigmaTaurtdPi2rtd
     ) public pure returns (uint256) {
-        uint256 q1 = FixedPointMathLib.exp(SignedMath.neg(r * tau)) / 2;
-        uint256 q2 = FixedPointMathLib.exp(SignedMath.neg(SignedMath.pow2(d2_) / 2));
-        uint256 denq2 = S.wmul(sigmaTaurtdPi2rtd);
+        uint256 e1 = FixedPointMathLib.exp(SignedMath.neg(r.wmul(tau)));
+        uint256 e2 = FixedPointMathLib.exp(SignedMath.neg(SignedMath.pow2(d2_) / 2));
+        uint256 den = S.wmul(sigmaTaurtdPi2rtd);
 
-        return q1.wmul(q2.wdiv(denq2));
+        return e1.wmul(e2).wdiv(den) / 2;
     }
 
     /// @dev N(d1) / 2K
     function c2(uint256 K, int256 d1_) public pure returns (uint256) {
-        return _normal(d1_).wdiv(2 * K);
+        return Normal.wcdf(d1_).wdiv(2 * K);
     }
 
     /// @dev e^-( d1^2 / 2) / Kσ√(2 pi t)
@@ -134,7 +140,7 @@ library Finance {
 
     /// @dev [ 1 / 2√(KS) x e^-(r / 2 + σ^2 / 8)t ] x N(d3)
     function c4(uint256 bo, int256 d3_) public pure returns (uint256) {
-        return (bo / 2).wmul(_normal(d3_));
+        return (bo / 2).wmul(Normal.wcdf(d3_));
     }
 
     /// @dev [ 1 / 2√(KS) x e^-(r / 2 + σ^2 / 8)t ] x [ e^-( d3^2 / 2) / o√(2 pi t) ]
@@ -145,24 +151,31 @@ library Finance {
         return bo.wmul(q2);
     }
 
-    /// @dev (r / 2 + σ^2 / 8)t
-    function _boexp(uint256 r, uint256 sigma, uint256 tau) private pure returns (uint256) {
-        return tau * (r / 2 + SignedMath.pow2(int256(sigma)) / 8);
+    /// @dev 1 / 2 K
+    function c6(uint256 K) private pure returns (uint256) {
+        return AmountsMath.wrap(1).wdiv(2 * K);
     }
 
+    /// @dev 1 / 2√(KS) x e^-(r / 2 + σ^2 / 8)t
+    function c7(uint256 bo) private pure returns (uint256) {
+        return bo / 2;
+    }
+
+    ////// HELPERS //////
+
     /// @dev √(KS)
-    function _kssqrd(uint256 K, uint256 S) private pure returns (uint256) {
+    function _ksrtd(uint256 K, uint256 S) private pure returns (uint256) {
         return FixedPointMathLib.sqrt(K.wmul(S));
+    }
+
+    /// @dev (r / 2 + σ^2 / 8)t
+    function _boexp(uint256 r, uint256 sigma, uint256 tau) private pure returns (uint256) {
+        return tau.wmul(r / 2 + SignedMath.pow2(int256(sigma)) / 8);
     }
 
     /// @dev 1 / √(KS) x e^-(r / 2 + σ^2 / 8)t
     function _bo(DeltaIGParams memory params) private pure returns (uint256) {
-        uint256 q1 = FixedPointMathLib.exp(SignedMath.neg(_boexp(params.r, params.sigma, params.tau)));
-        return q1.wdiv(_kssqrd(params.K, params.S));
-    }
-
-    /// @dev Utility to get the cdf of a signed value
-    function _normal(int256 n) private pure returns (uint256) {
-        return uint256(n);
+        uint256 e1 = FixedPointMathLib.exp(SignedMath.neg(_boexp(params.r, params.sigma, params.tau)));
+        return e1.wdiv(_ksrtd(params.K, params.S));
     }
 }
--- test/FinanceLib.t.sol
+++ test/FinanceLib.t.sol
@@ -14,82 +14,228 @@ import {Normal} from "../src/lib/Normal.sol";
 
 contract FinanceLibTest is Test {
     using AmountsMath for uint256;
+    uint256 V0 = 1e24; // 1,000,000 (1M)
     uint256 r = 2e16; // 0.02 or 2%
     uint256 sigma = 5e17; // 0.5 or 50%
 
-    function setUp() public {}
-
-    /// @dev Accepted delta on comparisons (up to 0.0001)
-    uint256 constant ERR = 1e14;
-
-    function testTermsDs_1() public {
-        uint256 K = 2e21; // 2000 strike price
-        uint256 S = 3e21; // 3000 current price
-        uint256 tau = WadTime.nYears(WadTime.daysFraction(5, 6)); // 5/6 of a day
-
-        (int256 d1, int256 d2, int256 d3, ) = Finance.ds(Finance.DeltaIGParams(r, sigma, K, S, tau));
-
-        assertApproxEqAbs(169854e14, d1, ERR);
-        assertApproxEqAbs(169615e14, d2, ERR);
-        assertApproxEqAbs(169734e14, d3, ERR);
+    struct TestCase {
+        // inputs
+        uint256 V0;
+        uint256 r;
+        uint256 sigma;
+        uint256 K;
+        uint256 S;
+        uint256 tau;
+        // results
+        int256 d1;
+        int256 d2;
+        int256 d3;
+        uint256 c1;
+        uint256 c2;
+        uint256 c3;
+        uint256 c4;
+        uint256 c5;
+        uint256 c6;
+        uint256 c7;
+        int256 igDUp;
+        int256 igDDown;
     }
 
-    function testTermsDs_2() public {
-        uint256 K = 2e21; // 2000 strike price
-        uint256 S = 1e17; // 0.1 current price
-        uint256 tau = WadTime.nYears(WadTime.daysFraction(4, 6)); // 4/6 of a day
+    mapping(uint256 => TestCase) testCases;
+    uint256 testCasesNum = 0;
 
-        (int256 d1, int256 d2, int256 d3, ) = Finance.ds(Finance.DeltaIGParams(r, sigma, K, S, tau));
+    /// @dev Accepted delta on comparisons (up to 1e-9)
+    uint256 constant ERR = 1e9;
 
-        assertApproxEqAbs(-4634454e14, d1, ERR);
-        assertApproxEqAbs(-4634668e14, d2, ERR);
-        assertApproxEqAbs(-4634561e14, d3, ERR);
-    }
-
-    function testTermsDs_3() public {
-        uint256 K = 2e21; // 2000 strike price
-        uint256 S = 15e20; // 1500 current price
-        uint256 tau = WadTime.nYears(WadTime.daysFraction(1, 2)); // 1/2 of a day
-
-        (int256 d1, int256 d2, int256 d3, ) = Finance.ds(Finance.DeltaIGParams(r, sigma, K, S, tau));
-
-        assertApproxEqAbs(-155347e14, d1, ERR);
-        assertApproxEqAbs(-155533e14, d2, ERR);
-        assertApproxEqAbs(-155440e14, d3, ERR);
-    }
-
-    function testTermsCs_1() public {
-        uint256 K = 2e21; // 2000 strike price
-        uint256 S = 3e21; // 3000 current price
-        uint256 tau = WadTime.nYears(WadTime.daysFraction(5, 6)); // 5/6 of a day
-
-        Finance.DeltaIGParams memory params = Finance.DeltaIGParams(r, sigma, K, S, tau);
-
-        (int256 d1, int256 d2, int256 d3, uint256 sigmaTaurtd) = Finance.ds(params);
-        uint256 sigmaTaurtdPi2rtd = sigmaTaurtd.wmul(Finance.PI2_RTD);
-        (uint256 c1, uint256 c2, uint256 c3, uint256 c4, uint256 c5) = Finance.cs(
-            params,
-            d1,
-            d2,
-            d3,
-            sigmaTaurtdPi2rtd
+    function setUp() public {
+        testCases[0] = TestCase(
+            V0,
+            r,
+            sigma,
+            2e21, // 2000 strike price
+            3e21, // 3000 current price
+            WadTime.nYears(WadTime.daysFraction(5, 6)), // 5/6 of a day
+            16985368844e9, // 16.98
+            16961477920e9, // 16.96
+            16973423382e9, // 16.97
+            0, // 0.000000000
+            250000e9, // 0.000250000
+            0, // 0.000000000
+            204105e9, // 0.000204105
+            0, // 0.000000000
+            250000e9, // 0.000250000
+            204105e9, // 0.000204105
+            45895077883e9,
+            0
         );
 
-        // console.log(c1);
-        // console.log(c2);
-        // console.log(c3);
-        // console.log(c4);
-        // console.log(c5);
+        testCases[1] = TestCase(
+            V0,
+            r,
+            sigma,
+            2e21, // 2000 strike price
+            1e17, // 0.1 current price
+            WadTime.nYears(WadTime.daysFraction(4, 6)), // 2/3 of a day
+            -463445429364e9, // -463.44
+            -463466798056e9, // -463.46
+            -463456113710e9, // -463.45
+            0, // 0.000000000
+            0, // 0.000000000
+            0, // 0.000000000
+            0, // 0.000000000
+            0, // 0.000000000
+            250000e9, // 0.000250000
+            35352675e9, // 0.035352675
+            0,
+            -35102675401250e9
+        );
+
+        testCases[2] = TestCase(
+            V0,
+            r,
+            sigma,
+            2e21, // 2000 strike price
+            15e20, // 1500 current price
+            WadTime.nYears(WadTime.daysFraction(3, 6)), // half of a day
+            -15534749771e9, // -15.53
+            -15553255601e9, // -15.55
+            -15544002686e9, // -15.54
+            0, // 0.000000000
+            0, // 0.000000000
+            0, // 0.000000000
+            0, // 0.000000000
+            0, // 0.000000000
+            250000e9, // 0.000250000
+            288659e9, // 0.000288659
+            0,
+            -38658822933e9
+        );
+
+        testCases[3] = TestCase(
+            V0,
+            r,
+            sigma,
+            2e21, // 2000 strike price
+            201e19, // 2010 current price
+            WadTime.nYears(WadTime.daysFraction(2, 6)), // 1/3 of a day
+            338847089e9, // 0.338847089,
+            323737142e9, // 0.323737142,
+            331292116e9, // 0.331292116,
+            6232393e9, // 0.006232393,
+            158159e9, // 0.000158159,
+            6232393e9, // 0.006232393,
+            157049e9, // 0.000157049,
+            12464787e9, // 0.012464787,
+            250000e9, // 0.000250000,
+            249368e9, // 0.000249368,
+            1110429038e9, // 1.110429038,
+            -478368890e9 // -0.478368890
+        );
+
+        testCases[4] = TestCase(
+            V0,
+            r,
+            sigma,
+            2e21, // 2000 strike price
+            199e19, // 1990 current price
+            WadTime.nYears(WadTime.daysFraction(1, 6)), // 1/3 of a day
+            -462951288e9, // -0.462951288
+            -473635634e9, // -0.473635634
+            -468293461e9, // -0.468293461
+            8386143e9, // 0.008386143
+            80425e9, // 0.000080425
+            8386143e9, // 0.008386143
+            80146e9, // 0.000080146
+            16772287e9, // 0.016772287
+            250000e9, // 0.000250000
+            250623e9, // 0.000250623
+            278957332e9, // 0.278957332
+            -901590214e9 // -0.901590214
+        );
+
+        testCases[5] = TestCase(
+            V0,
+            r,
+            sigma,
+            2e21, // 2000 strike price
+            2e21, // 2000 current price
+            WadTime.nYears(WadTime.daysFraction(1, 100)), // 1/100 of a day
+            0e9, // 0.000000000
+            0e9, // 0.000000000
+            0e9, // 0.000000000
+            38108887e9, // 0.038108887
+            125000e9, // 0.000125000
+            38108908e9, // 0.038108908
+            125000e9, // 0.000125000
+            76217730e9, // 0.076217730
+            250000e9, // 0.000250000
+            250000e9, // 0.000250000
+            65396203e9, // 0.065396203
+            -65113669e9 // -0.065113669
+        );
+
+        testCasesNum = 6;
     }
 
-    // function testDeltas() public {
-    //     uint256 K = 2e21; // 2000 strike price
-    //     uint256 S = 3e21; // 3000 current price
-    //     uint256 tau = WadTime.nYears(WadTime.daysFraction(5, 6)); // 5/6 of a day
+    function testTermsDs() public {
+        for (uint256 i = 0; i < testCasesNum; i++) {
+            (int256 d1, int256 d2, int256 d3, ) = Finance.ds(
+                Finance.DeltaIGParams(
+                    testCases[i].r,
+                    testCases[i].sigma,
+                    testCases[i].K,
+                    testCases[i].S,
+                    testCases[i].tau
+                )
+            );
 
-    //     (int256 igDUp, int256 igDDown) = Finance.igDeltas(r, sigma, K, S, tau);
+            assertApproxEqAbs(testCases[i].d1, d1, ERR);
+            assertApproxEqAbs(testCases[i].d2, d2, ERR);
+            assertApproxEqAbs(testCases[i].d3, d3, ERR);
+        }
+    }
 
-    //     assertApproxEqAbs(4590e16, igDUp, ERR);
-    //     assertApproxEqAbs(0, igDDown, ERR);
-    // }
+    function testTermsCs() public {
+        for (uint256 i = 0; i < testCasesNum; i++) {
+            Finance.DeltaIGParams memory params = Finance.DeltaIGParams(
+                testCases[i].r,
+                testCases[i].sigma,
+                testCases[i].K,
+                testCases[i].S,
+                testCases[i].tau
+            );
+
+            (int256 d1, int256 d2, int256 d3, uint256 sigmaTaurtd) = Finance.ds(params);
+            uint256 sigmaTaurtdPi2rtd = sigmaTaurtd.wmul(Finance.PI2_RTD);
+            Finance.DeltaIGComponents memory cs_ = Finance.cs(params, d1, d2, d3, sigmaTaurtdPi2rtd);
+
+            assertApproxEqAbs(testCases[i].c1, cs_.c1, ERR);
+            assertApproxEqAbs(testCases[i].c2, cs_.c2, ERR);
+            assertApproxEqAbs(testCases[i].c3, cs_.c3, ERR);
+            assertApproxEqAbs(testCases[i].c4, cs_.c4, ERR);
+            assertApproxEqAbs(testCases[i].c5, cs_.c5, ERR);
+            assertApproxEqAbs(testCases[i].c6, cs_.c6, ERR);
+            assertApproxEqAbs(testCases[i].c7, cs_.c7, ERR);
+        }
+    }
+
+    function testDeltas() public {
+        for (uint256 i = 0; i < testCasesNum; i++) {
+            Finance.DeltaIGParams memory params = Finance.DeltaIGParams(
+                testCases[i].r,
+                testCases[i].sigma,
+                testCases[i].K,
+                testCases[i].S,
+                testCases[i].tau
+            );
+
+            (int256 igDUp, int256 igDDown) = Finance.igDeltas(params);
+
+            igDUp = (int256(testCases[i].V0) * igDUp) / (10 ** 18);
+            igDDown = (int256(testCases[i].V0) * igDDown) / (10 ** 18);
+
+            assertApproxEqAbs(testCases[i].igDUp, igDUp, ERR);
+            assertApproxEqAbs(testCases[i].igDDown, igDDown, ERR);
+        }
+    }
 }
