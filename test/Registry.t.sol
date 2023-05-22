5e1b876c27342477236a88a85f2ad6ebe78c9f30
Damiano Enerli <damiano.enerli@loanxchain.com>
2023-05-22 17:16:06 +0200

    Minor fix registry test names

--- test/Registry.t.sol
+++ test/Registry.t.sol
@@ -5,9 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {Registry} from "../src/Registry.sol";
 
-
 contract RegistryTest is Test {
-
     bytes4 constant MissingAddress = bytes4(keccak256("MissingAddress()"));
     Registry registry;
 
@@ -15,34 +13,30 @@ contract RegistryTest is Test {
         registry = new Registry();
     }
 
+    function testNotRegisteredAddress() public {
+        address addrToCheck = address(0x150);
+        bool isAddressRegistered = registry.isRegistered(addrToCheck);
+        assertEq(isAddressRegistered, false);
+    }
 
-    function testFactoryRegisterAddress() public {
+    function testRegisterAddress() public {
         address addrToRegister = address(0x150);
         registry.register(addrToRegister);
         bool isAddressRegistered = registry.isRegistered(addrToRegister);
         assertEq(isAddressRegistered, true);
     }
 
-    function testFactoryNotRegisteredAddress() public {
-        address addrToCheck = address(0x150);
-        bool isAddressRegistered = registry.isRegistered(addrToCheck);
-        assertEq(isAddressRegistered, false);
-    }
-
-    function testFactoryUnregisterAddressFail() public {
+    function testUnregisterAddressFail() public {
         address addrToUnregister = address(0x150);
         vm.expectRevert(MissingAddress);
         registry.unregister(addrToUnregister);
     }
 
-    function testFactoryUnregisterAddress() public {
+    function testUnregisterAddress() public {
         address addrToUnregister = address(0x150);
         registry.register(addrToUnregister);
         registry.unregister(addrToUnregister);
-         bool isAddressRegistered = registry.isRegistered(addrToUnregister);
+        bool isAddressRegistered = registry.isRegistered(addrToUnregister);
         assertEq(isAddressRegistered, false);
-        
     }
-} 
-
-
+}
