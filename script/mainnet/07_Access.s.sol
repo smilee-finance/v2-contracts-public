e2bcf7402c6fdaa7972a6ea83d24cef3758effc0
Marco De Zio <marco@dvrs.studio>
2024-03-15 16:56:15 +0100

    Add script for managing roles within the protocol.

--- script/mainnet/07_Access.s.sol
+++ script/mainnet/07_Access.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.15;
 
 import {console} from "forge-std/console.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+import {IDVP} from "@project/interfaces/IDVP.sol";
 import {AddressProvider} from "@project/AddressProvider.sol";
 import {IG} from "@project/IG.sol";
 import {Vault} from "@project/Vault.sol";
 import {IGAccessNFT} from "@project/periphery/IGAccessNFT.sol";
 import {PositionManager} from "@project/periphery/PositionManager.sol";
+import {Registry} from "@project/periphery/Registry.sol";
 import {VaultAccessNFT} from "@project/periphery/VaultAccessNFT.sol";
 import {EnhancedScript} from "../utils/EnhancedScript.sol";
 
@@ -24,11 +27,13 @@ import {EnhancedScript} from "../utils/EnhancedScript.sol";
  */
 contract AccessTokenOps is EnhancedScript {
 
+    uint256 internal _godPrivateKey;
     uint256 internal _adminPrivateKey;
     AddressProvider internal _ap;
 
     constructor() {
         // Load the private key that will be used for signing the transactions:
+        _godPrivateKey = vm.envUint("GOD_PRIVATE_KEY");
         _adminPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
 
         string memory txLogs = _getLatestTransactionLogs("01_CoreFoundations.s.sol");
@@ -91,6 +96,88 @@ contract AccessTokenOps is EnhancedScript {
         vm.startBroadcast(_adminPrivateKey);
         vaultAccessNFT.createToken(user, limit);
         vm.stopBroadcast();
+    }
+
+    function grantRoleOnProtocol(string memory roleName, address account) public {
+        bytes32 role = keccak256(abi.encodePacked(roleName));
+
+        uint256 roleAdmin = 0;
+        if (role == keccak256("ROLE_GOD") || role == keccak256("ROLE_ADMIN")) {
+            roleAdmin = _godPrivateKey;
+        } else {
+            console.log("ROLE NAME NOT SUPPORTED");
+            return;
+        }
+
+        vm.startBroadcast(roleAdmin);
+
+        IAccessControl(address(_ap)).grantRole(role, account); // AddressProvider
+        IAccessControl(_ap.priceOracle()).grantRole(role, account); // ChainlinkPriceOracle
+        IAccessControl(_ap.marketOracle()).grantRole(role, account); // MarketOracle
+        IAccessControl(_ap.exchangeAdapter()).grantRole(role, account); // SwapAdapterRouter
+        IAccessControl(_ap.feeManager()).grantRole(role, account); // FeeManager
+        IAccessControl(_ap.registry()).grantRole(role, account); // Registry
+        IAccessControl(_ap.dvpPositionManager()).grantRole(role, account); // PositionManager
+        IAccessControl(_ap.vaultAccessNFT()).grantRole(role, account); // VaultAccessNFT
+        IAccessControl(_ap.dvpAccessNFT()).grantRole(role, account); // IGAccessNFT
+
+        string memory txLogs = _getLatestTransactionLogs("01_CoreFoundations.s.sol");
+        address uniswapAdapter = _readAddress(txLogs, "UniswapAdapter");
+        IAccessControl(uniswapAdapter).grantRole(role, account); // UniswapAdapter
+
+        Registry registry = Registry(_ap.registry());
+        address[] memory dvps = registry.getDVPs();
+        uint256 tot = dvps.length;
+        for (uint256 i = 0; i < tot; i++) {
+            address dvp = dvps[i];
+            address vault = IDVP(dvp).vault();
+            IAccessControl(dvp).grantRole(role, account); // IG
+            IAccessControl(vault).grantRole(role, account); // Vault
+        }
+
+        vm.stopBroadcast();
+        console.log("Number of DVPs with granted role:", tot);
+    }
+
+    function revokeRoleOnProtocol(string memory roleName, address account) public {
+        bytes32 role = keccak256(abi.encodePacked(roleName));
+
+        uint256 roleAdmin = 0;
+        if (role == keccak256("ROLE_GOD") || role == keccak256("ROLE_ADMIN")) {
+            roleAdmin = _godPrivateKey;
+        } else {
+            console.log("ROLE NAME NOT SUPPORTED");
+            return;
+        }
+
+        vm.startBroadcast(roleAdmin);
+
+        IAccessControl(address(_ap)).revokeRole(role, account); // AddressProvider
+        IAccessControl(_ap.priceOracle()).revokeRole(role, account); // ChainlinkPriceOracle
+        IAccessControl(_ap.marketOracle()).revokeRole(role, account); // MarketOracle
+        IAccessControl(_ap.exchangeAdapter()).revokeRole(role, account); // SwapAdapterRouter
+        IAccessControl(_ap.feeManager()).revokeRole(role, account); // FeeManager
+        IAccessControl(_ap.registry()).revokeRole(role, account); // Registry
+        IAccessControl(_ap.dvpPositionManager()).revokeRole(role, account); // PositionManager
+        IAccessControl(_ap.vaultAccessNFT()).revokeRole(role, account); // VaultAccessNFT
+        IAccessControl(_ap.dvpAccessNFT()).revokeRole(role, account); // IGAccessNFT
+
+        string memory txLogs = _getLatestTransactionLogs("01_CoreFoundations.s.sol");
+        address uniswapAdapter = _readAddress(txLogs, "UniswapAdapter");
+        IAccessControl(uniswapAdapter).revokeRole(role, account); // UniswapAdapter
+
+        Registry registry = Registry(_ap.registry());
+        address[] memory dvps = registry.getDVPs();
+        uint256 tot = dvps.length;
+        for (uint256 i = 0; i < tot; i++) {
+            address dvp = dvps[i];
+            address vault = IDVP(dvp).vault();
+            IAccessControl(dvp).revokeRole(role, account); // IG
+            IAccessControl(vault).revokeRole(role, account); // Vault
+        }
+
+        vm.stopBroadcast();
+        console.log("Number of DVPs with revoked role:", tot);
     }
 
 }
